4e8aa141c5261a7e790c4b9f849ce100de24b86c
luantrasel <luantrasel77@gmail.com>
2020-03-31 08:18:25 -0300

    products list

---
+++ frontend/src/pages/Home/index.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
+
+import { ProductList } from './styles';
+
+export default function Home() {
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/chuteira-futsal-nike-beco-2/72/HZM-0953-172/HZM-0953-172_detalhe1.jpg?ts=1581937308?ims=280x280"
+          alt="tenis"
+        />
+        <strong>tenis</strong>
+        <span>R$129,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/chuteira-futsal-nike-beco-2/72/HZM-0953-172/HZM-0953-172_detalhe1.jpg?ts=1581937308?ims=280x280"
+          alt="tenis"
+        />
+        <strong>tenis</strong>
+        <span>R$129,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/chuteira-futsal-nike-beco-2/72/HZM-0953-172/HZM-0953-172_detalhe1.jpg?ts=1581937308?ims=280x280"
+          alt="tenis"
+        />
+        <strong>tenis</strong>
+        <span>R$129,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/chuteira-futsal-nike-beco-2/72/HZM-0953-172/HZM-0953-172_detalhe1.jpg?ts=1581937308?ims=280x280"
+          alt="tenis"
+        />
+        <strong>tenis</strong>
+        <span>R$129,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/chuteira-futsal-nike-beco-2/72/HZM-0953-172/HZM-0953-172_detalhe1.jpg?ts=1581937308?ims=280x280"
+          alt="tenis"
+        />
+        <strong>tenis</strong>
+        <span>R$129,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/chuteira-futsal-nike-beco-2/72/HZM-0953-172/HZM-0953-172_detalhe1.jpg?ts=1581937308?ims=280x280"
+          alt="tenis"
+        />
+        <strong>tenis</strong>
+        <span>R$129,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
+}
