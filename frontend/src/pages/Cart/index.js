9f744723a90417bcbf1ca91ee70b6e68b2bbc0bb
luantrasel <luantrasel77@gmail.com>
2020-04-01 10:21:43 -0300

    reactotron configured, add to cart & remove from cart

--- frontend/src/pages/Cart/index.js
+++ frontend/src/pages/Cart/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
 import {
   MdRemoveCircleOutline,
   MdAddCircleOutline,
@@ -7,7 +9,7 @@ import {
 
 import { Container, ProductTable, Total } from './styles';
 
-export default function Cart() {
+function Cart({ cart, dispatch }) {
   return (
     <Container>
       <ProductTable>
@@ -21,37 +23,41 @@ export default function Cart() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>
-              <img
-                src="https://static.netshoes.com.br/produtos/chuteira-futsal-nike-beco-2/72/HZM-0953-172/HZM-0953-172_detalhe1.jpg?ts=1581937308?ims=280x280"
-                alt="Tênis"
-              />
-            </td>
-            <td>
-              <strong>Tenis</strong>
-              <span>R$129,90</span>
-            </td>
-            <td>
-              <div>
-                <button type="button">
-                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+          {cart.map((product) => (
+            <tr>
+              <td>
+                <img src={product.image} alt={product.title} />
+              </td>
+              <td>
+                <strong>{product.title}</strong>
+                <span>{product.priceFormatted}</span>
+              </td>
+              <td>
+                <div>
+                  <button type="button">
+                    <MdRemoveCircleOutline size={20} color="#7159c1" />
+                  </button>
+                  <input type="number" readOnly value={product.amount} />
+                  <button type="button">
+                    <MdAddCircleOutline size={20} color="#7159c1" />
+                  </button>
+                </div>
+              </td>
+              <td>
+                <strong>{product.price * product.amount}</strong>
+              </td>
+              <td>
+                <button
+                  type="button"
+                  onClick={() =>
+                    dispatch({ type: 'REMOVE_FROM_CART', id: product.id })
+                  }
+                >
+                  <MdDelete size={20} color="#7159c1" />
                 </button>
-                <input type="number" readOnly value={1} />
-                <button type="button">
-                  <MdAddCircleOutline size={20} color="#7159c1" />
-                </button>
-              </div>
-            </td>
-            <td>
-              <strong>R$258,80</strong>
-            </td>
-            <td>
-              <button type="button">
-                <MdDelete size={20} color="#7159c1" />
-              </button>
-            </td>
-          </tr>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </ProductTable>
 
@@ -66,3 +72,9 @@ export default function Cart() {
     </Container>
   );
 }
+
+const mapStateToProps = (state) => ({
+  cart: state.cart,
+});
+
+export default connect(mapStateToProps)(Cart);
--- frontend/src/store/modules/cart/reducer.js
+++ frontend/src/store/modules/cart/reducer.js
@@ -1,6 +1,28 @@
+import produce from 'immer';
+
 export default function cart(state = [], action) {
   if (action.type === 'ADD_TO_CART') {
-    return [...state, action.product];
+    return produce(state, (draft) => {
+      const productIndex = draft.findIndex((p) => p.id === action.product.id);
+
+      if (productIndex >= 0) {
+        draft[productIndex].amount += 1;
+      } else {
+        draft.push({
+          ...action.product,
+          amount: 1,
+        });
+      }
+    });
+  }
+  if (action.type === 'REMOVE_FROM_CART') {
+    return produce(state, (draft) => {
+      const productIndex = draft.findIndex((p) => p.id === action.id);
+
+      if (productIndex >= 0) {
+        draft.splice(productIndex, 1);
+      }
+    });
   }
 
   return state;
